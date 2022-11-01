a6c5f40e1f89bb3f5aac38c9609368fbcf5f53ea
sumedh <sumedhiscoding@gmail.com>
2022-11-01 22:49:08 +0530

    django template engine update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    # so the request object is going to be http object which tells us what kind of request method is going to be sent
+    # what kind of data is passed in what kind of data user is sending
+    return render(request,'home.html')
+
+def room(request):
+    return render(request,"room.html")
